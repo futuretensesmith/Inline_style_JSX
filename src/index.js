aec05848ff1f4a442ad26e9eb770768cd53bff00
Kevin Smith <futuretense@gmail.com>
2021-02-12 14:31:35 -0800

    Inline styles start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//since style is inline you have to write the style like a javascript object
+//{key: "value"} value has to be a string
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: " 1px solid black"
+};
+//update style on the fly
+customStyle.color = "blue";
 
 ReactDOM.render(
-  <h1 style={{ color: "red" }}>Hello World!</h1>,
+  <h1 style={customStyle}>Hello World!</h1>,
   document.getElementById("root")
 );
